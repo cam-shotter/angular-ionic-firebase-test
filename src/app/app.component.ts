7b8da553a429c5ff54dc04cd32e353d5a851b51d
Cam Shotter <cameronshotter@gmail.com>
2022-09-17 16:41:43 +1200

    added enum for icons and interface for Page type (might change the interface name later as it doesn't feel great). Added types to the app props

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,31 @@
 import { Component } from '@angular/core';
+
+export enum Icons {
+  addCircle = 'add-circle',
+  list = 'list',
+  calendar = 'calendar',
+  archive = 'archive'
+}
+
+export interface Page {
+  title: string,
+  url: string,
+  icon: Icons
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  public appPages = [
-    { title: 'Create', url: '/entries/Create', icon: 'add-circle' },
-    { title: 'All Entries', url: '/entries/All', icon: 'list' },
-    { title: 'Recent', url: '/entries/Recent', icon: 'calendar' },
-    { title: 'Archived', url: '/entries/Archived', icon: 'archive' },
+  public appPages: Page[] = [
+    { title: 'Create', url: '/entries/Create', icon: Icons.addCircle },
+    { title: 'All Entries', url: '/entries/All', icon: Icons.list },
+    { title: 'Recent', url: '/entries/Recent', icon: Icons.calendar },
+    { title: 'Archived', url: '/entries/Archived', icon: Icons.archive },
   ];
-  public labels = ['Important', 'Personal', 'Notes', 'Work', 'Reminders'];
+  public labels: string[] = ['Important', 'Personal', 'Notes', 'Work', 'Reminders'];
+
   constructor() {}
 }
