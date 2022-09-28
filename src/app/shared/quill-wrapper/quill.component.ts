a992507f7049beb9b4ff91174dddd6164af7c3a1
Cam Shotter <cameronshotter@gmail.com>
2022-09-28 23:11:07 +1300

    forgot to unsubscribe from the subscription here, needs to be done on this because it's not subscribing reactively through the async pipe

--- src/app/shared/quill-wrapper/quill.component.ts
+++ src/app/shared/quill-wrapper/quill.component.ts
@@ -1,21 +1,23 @@
-import { Component, Input, OnInit, ViewChild } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { ContentChange, QuillEditorComponent } from 'ngx-quill';
-import { BehaviorSubject, debounceTime, distinctUntilChanged, Subject } from 'rxjs';
+import { BehaviorSubject, debounceTime, distinctUntilChanged, Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-quill',
   templateUrl: './quill.component.html',
   styleUrls: ['./quill.component.scss'],
 })
-export class QuillComponent implements OnInit {
+export class QuillComponent implements OnInit, OnDestroy {
   @Input() editorPlaceholder = 'Placeholder';
 
   @ViewChild('editor', {
     static: true
   }) editor: QuillEditorComponent
 
-  contentSubject = new BehaviorSubject(this.editorPlaceholder);
-  editorContent = this.contentSubject.asObservable();
+  private contentSubject = new BehaviorSubject(this.editorPlaceholder);
+  editorContent$ = this.contentSubject.asObservable();
+
+  private readonly destroy$ = new Subject<void>();
 
   constructor() {}
 
@@ -23,11 +25,17 @@ export class QuillComponent implements OnInit {
     this.editor
       .onContentChanged
       .pipe(
+        takeUntil(this.destroy$),
         debounceTime(400),
         distinctUntilChanged(),
       )
       .subscribe((data: ContentChange) => {
         this.contentSubject.next(data.html)
       })
+  }
+
+  ngOnDestroy() {
+    this.destroy$.next();
+    this.destroy$.complete();
   }
 }
